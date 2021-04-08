9e0997136b23745ce0b08db36fb0f8e218293571
Shreyaan Vashishtha <77964841+ShreyaanVashishtha@users.noreply.github.com>
2021-04-08 22:02:04 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <template>
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
+      <h1 class="title text-center">Shreyaan.co</h1>
+      <h2 class="subtitle text-center">You’ve arrived at this page because you typed or linked to “shreyaan.co”, Shreyaan’s official URL shortcut just for Shreyaan's websites.
+
+Whenever you see a short “shreyaan.co” link, you can trust that it will always take you to a trusted product or service.</h2>
     </section>
 
-    <section class="mt-8">
-      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
-      <posts post-type="blog" :amount="1" />
-    </section>
   </main>
 </template>
