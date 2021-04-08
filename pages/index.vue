e72021806c1c7be1fc206ece1cda2725b7e55dc4
Shreyaan Vashishtha <77964841+ShreyaanVashishtha@users.noreply.github.com>
2021-04-08 22:05:10 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
       <h1 class="title text-center">Shreyaan.co</h1>
-      <h2 class="subtitle text-center">You’ve arrived at this page because you typed or linked to “shreyaan.co”, Shreyaan’s official URL shortcut just for Shreyaan's websites.
-
-Whenever you see a short “shreyaan.co” link, you can trust that it will always take you to a trusted product or service.</h2>
+      <h2 class="subtitle text-center">You’ve arrived at this page because you typed or linked to “shreyaan.co”, Shreyaan’s official URL shortcut just for Shreyaan's websites.</h2>
+<br></br>
+<h2 class="subtitle text-center">Whenever you see a short “shreyaan.co” link, you can trust that it will always take you to a trusted product or service.</h2>
     </section>
 
   </main>
